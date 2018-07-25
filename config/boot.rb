872577f42569a43f15a007afd2f0ffdde5396caa
ghayn <ghayn@outlook.com>
2018-07-25 23:11:08 +0800

    update env and ci configs

--- config/boot.rb
+++ config/boot.rb
@@ -4,18 +4,18 @@ require "json"
 
 Bundler.require(:default, ENV['RACK_ENV'].to_sym)
 
+Dotenv.overload ".env.#{ENV['RACK_ENV']}"
+
 puts "Connect Postgres DB..."
 Sequel::Model.db = Sequel.connect(
   adapter: :postgres,
   user: ENV["POSTGRES_USER"],
   password: ENV["POSTGRES_PASSWORD"],
-  host: 'db',
+  host: ENV["POSTGRES_HOST"],
   port: ENV['POSTGRES_PORT'],
   max_connections: ENV["THREAD_COUNT"],
   database: "#{ENV['POSTGRES_DATABASE_BASE_NAME']}_#{ENV["RACK_ENV"]}"
 )
 puts "Connect Postgres DB complete."
-
-Dotenv.load ".env.#{ENV['RACK_ENV']}"
 
 Dir["./app/**/*.rb"].each { |file| require file }
